60edc0d14cb427902142857784edc45ba7cd996d
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2016-12-25 21:10:39 +0100

    embedded: update animations list

--- embedded24hc17/AnimManager.cpp
+++ embedded24hc17/AnimManager.cpp
@@ -1,16 +1,30 @@
 #include "AnimManager.h"
 
 #include "anim__TicTac.h"
+#include "anim__Circles.h"
+#include "anim__FillAll.h"
+#include "anim__FillSpiral.h"
+#include "anim__RotBeacon.h"
+#include "anim__Breathing.h"
+#include "anim__HuePan.h"
+#include "anim__HeartBeat.h"
 
 namespace {
 	union {
 		anim::TicTac::Data a00;
+		anim::Circles::Data a01;
+		anim::FillAll::Data a02;
+		anim::FillSpiral::Data a03;
+		anim::RotBeacon::Data a04;
+		anim::Breathing::Data a05;
+		anim::HuePan::Data a06;
+		anim::HeartBeat::Data a07;
 	} anim_data;
 }
 
 CRGB AnimManager::leds[nb_leds];
 
-void (*AnimManager::play_ptr)(void*) = &anim::TicTac::play;
+void (*AnimManager::play_ptr)(void*) = &anim::Breathing::play;
 
 void AnimManager::setup() {
 	FastLED.addLeds<NEOPIXEL, neopixel_datapin>(leds, nb_leds);
@@ -19,8 +33,8 @@ void AnimManager::setup() {
 void AnimManager::set(int anim) {
 	switch (anim) {
 		default:
-			anim::TicTac::init_data(&anim_data);
-			play_ptr = &anim::TicTac::play;
+			anim::Breathing::init_data(&anim_data);
+			play_ptr = &anim::Breathing::play;
 	}
 }
 
