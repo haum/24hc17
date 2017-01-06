62f78dc9965e4385e883a857db19b4d78ca193f0
LodeRunner <lode.runner@laposte.net>
2017-01-06 13:07:51 +0100

    embedded: nouvelle version de anim__Circles

--- embedded24hc17/anim__Circles.cpp
+++ embedded24hc17/anim__Circles.cpp
@@ -1,44 +1,33 @@
 #include "anim__Circles.h"
 
-namespace anim {
-namespace Circles {
+namespace anim 
+{
+namespace Circles 
+{
 
-	void init_data(void* p) {
+	void init_data(void* p) 
+	{
 		auto d = static_cast<Data*>(p);
 		d->divider = 0;
-		d->step = 0;
+		d->t = 0;
 		AnimManager::clear();
 	}
 
-	void play(void* p) {
-		const constexpr int wait = 1000;
-		const constexpr int circles[3][4] = {
-			{0, 5, 6, 12},
-			{1, 4, 7, 11},
-			{2, 3, 8, 10}
-		};
+	void play(void* p) 
+	{
+		
 		auto d = static_cast<Data*>(p);
-		int ii;
-		if (d->divider == wait/10) {
-			d->divider = 0;
-			switch (d->step) {
-				case 3:
-					AnimManager::setLed(9, CRGB::White);
-					break;
-				case 4:
-					for (ii = 0; ii < 13; ++ii) {
-						AnimManager::setLed(ii, CRGB::Black);
-					}
-					break;
-				default:
-					for (ii = 0; ii < 4; ++ii) {
-						AnimManager::setLed(circles[d->step][ii], CRGB::White);
-					}
-			}
-			d->step = d->step==4 ? 0 : d->step+1;
-		} else {
-			d->divider++;
-		}
+		d->t++;
+		uint8_t scaler, r, g, b;
+		CRGB color;
+		
+		scaler = triwave8(d->t);
+		r = scaler;
+		g = scaler;
+		b = scaler;
+		color = CRGB(r, g, b);
+		AnimManager::setAllLeds(color);
+		
 	}
 
 }
--- embedded24hc17/anim__Circles.h
+++ embedded24hc17/anim__Circles.h
@@ -1,12 +1,17 @@
 #include "AnimManager.h"
 
-namespace anim {
-namespace Circles {
-	struct Data {
-		int divider;
-		int step;
-	};
-	void init_data(void * data);
-	void play(void * data);
-}
+namespace anim 
+{
+	namespace Circles 
+	{
+	
+		struct Data 
+			{
+				int divider;
+				uint8_t t;
+			};
+	
+		void init_data(void * data);
+		void play(void * data);
+	}
 }
