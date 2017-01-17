06966d71cb44ea0886763044d9e598f5862635b0
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2017-01-17 23:11:00 +0100

    serverside: update models to ease the views' writting

--- serverside/Urbs/UrbsFrontend/models.py
+++ serverside/Urbs/UrbsFrontend/models.py
@@ -1,4 +1,68 @@
 from django.db import models as M
+from .encodedstate import EncodedState
+
+class Animation(M.Model):
+    """ Animation in the Laumio
+    name -- animation name
+    index -- index in the Laumio's table
+    """
+
+    name = M.TextField(max_length=100)
+    index = M.IntegerField(default=0)
+    params = M.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Challenge(M.Model):
+    """ Challenge
+
+    name -- challenge's name
+    description -- description of the challenge
+    index -- index of the challenge on the laumio
+    """
+
+    name = M.TextField(max_length=200)
+    description = M.TextField(max_length=1000, blank=True, null=True)
+    index = M.IntegerField(default=0)
+    params = M.IntegerField(default=0)
+    is_jail = M.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    def asdict(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'index': self.index
+        }
+
+
+class Step(M.Model):
+    """
+    A step in the line of riddles
+    """
+    index = M.IntegerField(null=True, blank=True)
+    challenge = M.ForeignKey(Challenge)
+    animation = M.ForeignKey(Animation)
+    jails = M.ManyToManyField(Challenge, blank=True, related_name='jails')
+    is_jail = M.BooleanField(default=False)
+
+    def __str__(self):
+        jailcount = self.jails.count()
+        return "Step %d using challenge %s (%d jails)"%(self.index, str(self.challenge), jailcount)
+
+    def asdict(self, challenges=True):
+        dictrepr = {
+            'index': self.index,
+        }
+        if challenges:
+            dictrepr['challenge'] = self.challenge.asdict()
+            dictrepr['jails'] = [_.asdict() for _ in self.jails.all()]
+        return dictrepr
+
 
 class Team(M.Model):
     """ Team
@@ -10,20 +74,24 @@ class Team(M.Model):
 
     name = M.TextField(max_length=200, unique=True)
     location = M.TextField(max_length=200, default='')
+    laumio = M.TextField(max_length=4, default='')
     score = M.IntegerField(default=0)
+    jail = M.BooleanField(default=False)
+    step = M.ForeignKey(Step, null=True)
+    challenge = M.ForeignKey(Challenge, null=True)
 
     def __str__(self):
         return self.name
-
 
     def asdict(self, members=False):
         dictrepr = {
             'name': self.name,
             'location': self.location,
-            'score': self.score
+            'score': self.score,
+            'laumio': self.laumio
         }
         if members:
-            dictrepr['members'] = [ _.asdict(team=False) for _ in self.member_set.filter(primary=True)]
+            dictrepr['members'] = [_.asdict(team=False) for _ in self.member_set.filter(primary=True)]
         return dictrepr
 
 
@@ -43,10 +111,9 @@ class Member(M.Model):
 
     def __str__(self):
         if self.primary:
-            return "%s (%s)"%(self.pseudo,self.team)
+            return "%s (%s)"%(self.pseudo, self.team)
         else:
-            return "%s aka. %s (%s)"%(self.pseudo,self.mainentry, self.team)
-
+            return "%s aka. %s (%s)"%(self.pseudo, self.mainentry, self.team)
 
     def asdict(self, team=True):
         """Return a dict representation of the object"""
@@ -62,41 +129,10 @@ class Member(M.Model):
         return dictrepr
 
 
-class Challenge(M.Model):
-    """ Challenge
-
-    name -- challenge's name
-    description -- description of the challenge
-    index -- index of the challenge on the laumio
-    """
-
-    name = M.TextField(max_length=200)
-    description = M.TextField(max_length=1000, blank=True, null=True)
-    index = M.IntegerField(default=0)
-
-    def __str__(self):
-        return self.name
-
-
-class Step(M.Model):
-    """
-    A step in the line of riddles
-    """
-    index = M.IntegerField(null=True, blank=True)
-    challenge = M.ForeignKey(Challenge)
-    next_challenge = M.ForeignKey('self', blank=True, null=True)
-    jails = M.ManyToManyField(Challenge, blank=True, related_name='jails')
-
-    def __str__(self):
-        jailcount = self.jails.count()
-        return "Step %d using challenge %s (%d jails)"%(self.index, str(self.challenge), jailcount)
-
-
 class Attempt(M.Model):
     """ Attempt to a Challenge
 
     team -- responsible team
-    leaf -- answered challenge (RTLeaf)
     token_in -- token sent to unlock (from team)
     token_out -- token sent back to the team
     faults -- number of faults
@@ -106,14 +142,40 @@ class Attempt(M.Model):
 
     timestamp = M.DateTimeField(auto_now_add=True)
     step = M.ForeignKey(Step)
+    challenge = M.ForeignKey(Challenge)
     jail = M.BooleanField(default=False)
-    token_in = M.TextField(max_length=100)
-    token_out = M.TextField(max_length=100)
+    success = M.BooleanField(default=False)
+    team = M.ForeignKey(Team)
+    submitter = M.ForeignKey(Member)
+    token_in = M.TextField(max_length=22)
+    token_out = M.TextField(max_length=22)
     faults = M.IntegerField(default=0)
     points = M.IntegerField(default=0)
 
     def __str__(self):
-        "Attempt from %s at %s on %s"%(self.team, self.timestamp, self.leaf)
+        return "Attempt from %s at %s on %s"%(self.team, self.timestamp, self.step)
+
+    def decode_token_to_dict(self, token_str):
+        return EncodedState().from_string(token_str).D
+
+    def asdict(self, team=True):
+        return {
+            'date': self.timestamp.timestamp(),
+            'points': self.points,
+            'step': self.step.asdict(),
+            'faults': self.faults,
+            'submitter': self.submitter.asdict(team=team),
+            'tokens': {
+                'in': {
+                    'as_str': self.token_in,
+                    'decoded': self.decode_token_to_dict(self.token_in)
+                },
+                'out': {
+                    'as_str': self.token_out,
+                    'decoded': self.decode_token_to_dict(self.token_out)
+                }
+            },
+        }
 
 
 class MessageType(M.Model):
@@ -142,14 +204,3 @@ class Message(M.Model):
         return "(%s) %s"%(self.type, self.message)
 
 
-class Animation(M.Model):
-    """ Animation in the Laumio
-    name -- animation name
-    index -- index in the Laumio's table
-    """
-
-    name = M.TextField(max_length=100)
-    index = M.IntegerField(default=0)
-
-    def __str__(self):
-        return self.name
