68d76591b34e276cff4721af1d417dd544543728
neodarz <neodarz@neodarz.net>
2017-01-18 12:22:52 +0100

    embedded: fix the difference enigma

--- embedded24hc17/scripts/Difference/gen.py
+++ embedded24hc17/scripts/Difference/gen.py
@@ -1,8 +1,5 @@
-#Usage exemple: python gen.py code24hc17 2
-#The file output is enigma2 and enigma2
-
+from random import randint, shuffle
 from sys import stdout
-from random import randint
 from sys import argv
 import os
 
@@ -27,71 +24,27 @@ if (argument_test == 1):
     enigma = open(enigma_path_file, 'w')
     answer = open(answer_path_file, 'w')
 
-U0 = randint(-100,100)
-U1 = randint(-100,100)
-V0 = randint(-100,100)
-V1 = randint(-100,100)
+ru = randint(-100,100)
+rv = randint(-100,100)
+u0 = randint(-100,100)
+v0 = randint(-100,100)
 
-list_u = list()
-list_v = list()
-#stdout.write(str(list_V[0]))
 
-r = U1-U0
-v = V1-V0
-if (argument_test == 1):
-    answer.write("r: "+str(r)+" ")
-    answer.write("v: "+str(v)+"\n")
-if (argument_test == 1):
-    answer.close()
+u = list(u0+i*ru for i in range(50))
+v = list(v0+i*rv for i in range(50))
 
-UN = U0
-UN1 = UN+r
-stdout.write("r: "+str(r)+"\n")
-#if (argument_test == 1):
-#    enigma.write(str(UN1)+" ")
+z = u+v
+shuffle(z)
 
-VN = V0
-VN1 = VN+v
-stdout.write("v: "+str(v)+"\n")
-
-list_u.insert(0, UN1)
-stdout.write("Suite u: "+str(list_u[0])+" ")
-UN = UN1
-# On comment obligatoirement à partir de 1 ! Car U0 est déjà calculer au dessus
-for i in range(1,100):
-    UN1 = UN+r
-    list_u.insert(i, UN1)
-    stdout.write(str(list_u[i])+" ")
-    #if (argument_test == 1):
-    #    enigma.write(str(UN1)+" ")
-    UN = UN1
-
-stdout.write("\n")
-
-list_v.insert(0, VN1)
-stdout.write("Suite v: "+str(list_v[0])+" ")
-VN = VN1
-for z in range(1,100):
-    VN1 = VN+v
-    list_v.insert(z, VN1)
-    stdout.write(str(list_v[z])+" ")
-    VN = VN1
-
-stdout.write("\n")
-
-stdout.write("Mix: ")
-
-for p in range(0,100):
-    if (p % 2 == 0):
-        stdout.write(str(list_v[p])+" ")
-        if (argument_test == 1):
-            enigma.write(str(list_v[p])+" ")
-    else:
-        stdout.write(str(list_u[p])+" ")
-        if (argument_test == 1):
-            enigma.write(str(list_u[p])+" ")
+for o in z:
+    stdout.write(str(o)+" ")
+    if (argument_test == 1):
+        enigma.write(str(o)+" ")
 
 if (argument_test == 1):
     enigma.write("\n")
+stdout.write("\nru: "+str(ru)+";rv: "+str(rv))
+if (argument_test == 1):
+    answer.write("ru: "+str(ru)+" "+";rv: "+str(rv)+"\n")
+    answer.close()
     enigma.close()
-stdout.write("\n")
