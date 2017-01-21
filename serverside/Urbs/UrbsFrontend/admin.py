f6712666aec8356776e2a6350353adcfdab541ce
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2017-01-21 15:33:37 +0100

    serverside: improve admin

--- serverside/Urbs/UrbsFrontend/admin.py
+++ serverside/Urbs/UrbsFrontend/admin.py
@@ -9,13 +9,39 @@ class StepAdmin(admin.ModelAdmin):
     model = Step
     list_display = ['index', '__str__']
 
+class AttemptAdmin(admin.ModelAdmin):
+
+    model = Attempt
+    list_filter = ['team__name', 'submitter__pseudo']
+
+class MemberAdmin(admin.ModelAdmin):
+
+    model = Member
+    list_display = ['pseudo', 'team', 'related_teamlaumio']
+    list_filter = ['team']
+
+    def related_teamlaumio(self, obj):
+        return obj.team.laumio
+    related_teamlaumio.short_description = "Laumio's ID"
+
+class TeamAdmin(admin.ModelAdmin):
+
+    model = Team
+    list_display = ['name', 'score', 'laumio', 'step', 'related_stepindex']
+
+    def related_stepindex(self, obj):
+        return obj.step.index
+    related_stepindex.short_description = "Position in tree"
+
+
+
 
 # admin.site.register(Team, TeamAdmin)
 admin.site.register(Animation)
 admin.site.register(Challenge)
-admin.site.register(Team)
+admin.site.register(Team, TeamAdmin)
 admin.site.register(Step, StepAdmin)
-admin.site.register(Member)
-admin.site.register(Attempt)
+admin.site.register(Member, MemberAdmin)
+admin.site.register(Attempt, AttemptAdmin)
 admin.site.register(MessageType)
 admin.site.register(Message)
