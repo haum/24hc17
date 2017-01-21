c83642f3d75f41eb620ddeed9fadd272b8bc392e
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2017-01-21 13:20:04 +0100

    serverside: order steps

--- serverside/Urbs/UrbsFrontend/admin.py
+++ serverside/Urbs/UrbsFrontend/admin.py
@@ -2,14 +2,19 @@ from django.contrib import admin
 
 from .models import Animation, Challenge, Step, Team, Member, Attempt, MessageType, Message
 
-# class TeamAdmin(admin.ModelAdmin):
+# # class TeamAdmin(admin.ModelAdmin):
+
+class StepAdmin(admin.ModelAdmin):
+
+    model = Step
+    list_display = ['index', '__str__']
 
 
 # admin.site.register(Team, TeamAdmin)
 admin.site.register(Animation)
 admin.site.register(Challenge)
-admin.site.register(Step)
 admin.site.register(Team)
+admin.site.register(Step, StepAdmin)
 admin.site.register(Member)
 admin.site.register(Attempt)
 admin.site.register(MessageType)
